10eb5a0bdaae42e13eb00415ede7f4afa171b209
Balachandran <balachandran2729@gmail.com>
2025-06-22 12:23:43 +0530

    first commit

--- components/Footer.js
+++
@@ -1,7 +0,0 @@
-const Footer = () => (
-  <footer className="bg-dark text-gray-400 py-4 text-center text-sm">
-    © {new Date().getFullYear()} Balachander. All rights reserved.
-  </footer>
-);
-
-export default Footer;
